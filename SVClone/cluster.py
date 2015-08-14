Fixed divide by zero but in copy-number normalisation. Extended trace plotting y axis to 2. Fixed issue with matplotlib X default. Adjust phi subtraction to be non-negative.

SVClone/cluster.py
@@ -23,8 +23,8 @@ def normalise_wins_by_cn(df_flt):
 
     bp1_nonzero = np.logical_not(bp1_wcn==0)
     bp2_nonzero = np.logical_not(bp2_wcn==0)
-    bp1_win[bp1_nonzero] = (bp1_win/bp1_wcn)[bp1_nonzero]
-    bp2_win[bp2_nonzero] = (bp2_win/bp2_wcn)[bp2_nonzero]
+    bp1_win[bp1_nonzero] = (bp1_win[bp1_nonzero]/bp1_wcn[bp1_nonzero])
+    bp2_win[bp2_nonzero] = (bp2_win[bp2_nonzero]/bp2_wcn[bp2_nonzero])
     
     return bp1_win,bp2_win
 
@@ -151,8 +151,10 @@ def fit_and_sample(model, iters, burn, thin):
     return mcmc
 
 def get_pv(phi,cn_r,cn_v,mu,pi):
+    rem = 1.0 - phi
+    rem = rem.clip(min=0)
     pn =  (1.0 - pi) * 2            #proportion of normal reads coming from normal cells
-    pr =  pi * (1.0 - phi) * cn_r   #proportion of normal reads coming from other clusters
+    pr =  pi * rem * cn_r   #proportion of normal reads coming from other clusters
     pv =  pi * phi * cn_v           #proportion of variant + normal reads coming from this cluster
 
     norm_const = pn + pr + pv

SVClone/run_clus.py
@@ -7,6 +7,8 @@ import os
 import pandas as pd
 import scipy as sp
 import scipy.stats
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import colorsys
 import IPython
@@ -62,7 +64,7 @@ def plot_clusters(center_trace,clusters,assignments,df,pl,pi,rlen,clus_out_dir,a
 
     RGB_tuples = gen_new_colours(len(clusters))
 
-    axes[0].set_ylim([0,1])
+    axes[0].set_ylim([0,2])
     axes[0].set_title("Trace of $\phi_k$")
 
     for idx,clus in enumerate(clusters):