- precision factor fix to avoid ZeroProbability error - coclustering of SNVs/SVs - output filtered SNVs post filtering - fixed some output headers/ensured output of proportions not CCFs

SVClone/cluster.py
@@ -156,11 +156,13 @@ def get_sv_vals(df,rlen):
     dep = np.array(norm+sup,dtype=float)
 
     #return sup,dep,cn_r,cn_v,mu_v,sides,Nvar
-    return sup,dep,combos,sides,Nvar
+    cn_states = [cn[side] for cn,side in zip(combos,sides)]
+    return sup,dep,cn_states,Nvar
 
 def get_snv_vals(df):
     n = df['ref'].values
     b = df['var'].values
+
     #cn_r,cn_v,mu_v = [],[],[]
     df = df.fillna('')
     df = df[df.chrom.values!='']
@@ -175,7 +177,6 @@ def get_snv_vals(df):
 #        cn_r.append([cnr])
 #        cn_v.append([cnv])
 #        mu_v.append([mu])
-        
     return b,(n+b),combos,len(b)
 
 def fit_and_sample(model, iters, burn, thin, use_map):
@@ -250,30 +251,37 @@ def get_most_likely_cn_states(cn_states,s,d,phi,pi):
     cn_ll = [ calc_lik(cn_states[i],s[i],d[i],phi[i],pi)  for i in range(len(cn_states)) ]
     most_likely_pv = [ cn_lik[0][np.where(np.nanmax(cn_lik[1])==cn_lik[1])[0][0]] for i,cn_lik in enumerate(cn_ll)]
     most_likely_cn = [ cn_states[i][np.where(np.nanmax(cn_lik[1])==cn_lik[1])[0][0]] for i,cn_lik in enumerate(cn_ll)]
-    return most_likely_cn, most_likely_pv
+    #TODO: currently using precision fudge factor to get around 
+    #0 probability errors when pv = 1 - look into this bug more
+    return most_likely_cn, np.array(most_likely_pv)-0.00000001
 
-def cluster(df,pi,rlen,insert,ploidy,iters,burn,thin,beta,use_map,are_snvs=False,Ndp=param.clus_limit):
+def cluster(sv_df,snv_df,pi,rlen,insert,ploidy,iters,burn,thin,beta,use_map,Ndp=param.clus_limit):
     '''
     clustering model using Dirichlet Process
     '''
     pl = ploidy
-    #sup,dep,cn_r,cn_v,mu_v = [],[],[],[],[]
     sup, dep, cn_states = [], [], []
-    sides = [] #only relevant for SVs
+    #sides = [] #only relevant for SVs
     cn_states = []
     Nvar = 0
     
-    if are_snvs:        
-        #sup,ref,cn_r,cn_v,mu_v = get_snv_vals(df)
-        sup,dep,cn_states,Nvar = get_snv_vals(df)
-        av_cov = np.mean(dep)
-        sides = np.zeros(Nvar,dtype=int)
+    if len(sv_df)>0 and len(snv_df)>0:
+        print("Coclustering SVs & SNVs...")
+        sup,dep,cn_states,Nvar = get_snv_vals(snv_df)
+        sv_sup,sv_dep,sv_cn_states,sv_Nvar = get_sv_vals(sv_df,rlen)
+        
+        sup = np.append(sup,sv_sup)
+        dep = np.append(dep,sv_dep)
+        cn_states = np.append(cn_states,sv_cn_states)
+        Nvar = Nvar + sv_Nvar
+    elif len(snv_df)>0:
+        print("Clustering SNVs...")
+        sup,dep,cn_states,Nvar = get_snv_vals(snv_df)
+        #sides = np.zeros(Nvar,dtype=int)
     else:
-        #sup,dep,cn_r,cn_v,mu_v,sides,Nvar = get_sv_vals(df,rlen)
-        #cn_states = get_cn_states(cn_r,cn_v,mu_v,sides)
-        sup,dep,combos,sides,Nvar = get_sv_vals(df,rlen)
-        cn_states = [cn[side] for cn,side in zip(combos,sides)]
-    
+        print("Clustering SVs...")
+        sup,dep,cn_states,Nvar = get_sv_vals(sv_df,rlen)
+
     sens = 1.0 / ((pi/float(pl))*np.mean(dep))
     alpha = pm.Gamma('alpha',0.9,1/0.9,value=2)
     #beta = pm.Gamma('beta',param.beta_shape,param.beta_rate)

SVClone/cmd.py
@@ -9,7 +9,8 @@ import numpy as np
 
 parser = argparse.ArgumentParser(prefix_chars='--')
 parser.add_argument("-s","--samples",dest="samples",
-    help="Required: Sample names (comma separated if multiple), not including germline.")
+        help="Required: Sample names (comma separated if multiple), not including germline. " + \
+             "WARNING: if clustering using mutect SNVs, the sample name must match the sample name in the vcf file. ")
 parser.add_argument("-i","--input",dest="procd_svs",
     help="Required: Processed structural variation input (comma separated if multiple).")
 parser.add_argument("-g","--germline",dest="germline",default="",
@@ -47,12 +48,14 @@ parser.add_argument("--beta",dest="beta",default="8,1/0.05,0.1",type=str,
              "Dirichlet Processes' gamma function. Third value determines the starting value.")
 parser.add_argument("--snvs",dest="snvs",default="",type=str,
         help="SNVs in VCF format to (optionally) compare the clustering with SVs.")
-parser.add_argument("--snv-format",dest="snv_format",default="sanger",type=str,
+parser.add_argument("--snv_format",dest="snv_format",default="sanger",type=str,
         help="Supplied SNV VCF is in the following input format: sanger (default), mutect or mutect_callstats.")
-parser.add_argument("--merge-clusts",dest="merge_clusts",action="store_true",
+parser.add_argument("--merge",dest="merge_clusts",action="store_true",
         help="Set to merge clusters.")
 parser.add_argument("--map",dest="use_map",action="store_true",
         help="Use maximum a-posteriori fitting (may significantly increase runtime).")
+parser.add_argument("--cocluster",dest="cocluster",action="store_true",
+        help="Whether to cluster SNVs and SVs together.")
 
 args = parser.parse_args()
 
@@ -76,6 +79,7 @@ snvs    = args.snvs
 use_map = args.use_map
 snv_format      = args.snv_format
 merge_clusts    = args.merge_clusts
+cocluster       = args.cocluster
 
 def proc_arg(arg,n_args=1,of_type=str):
     arg = str.split(arg,',')
@@ -108,4 +112,4 @@ if __name__ == '__main__':
         snv_format = snv_format.lower()
         if not np.any(snv_format==np.array(["sanger","mutect","mutect_callstats"])):
             raise ValueError("Invalid VCF format specified")
-    run.run(samples,svs,gml,cnvs,rlen,insert,pi,ploidy,out,n_runs,n_iter,burn,thin,beta,neutral,snvs,snv_format,merge_clusts,use_map)
+    run.run(samples,svs,gml,cnvs,rlen,insert,pi,ploidy,out,n_runs,n_iter,burn,thin,beta,neutral,snvs,snv_format,merge_clusts,use_map,cocluster)

SVClone/run.py
@@ -223,7 +223,6 @@ def load_snvs_mutect_callstats(snvs):
     return snv_out
     
 def load_snvs_mutect(snvs,sample):
-    #TODO: remove sample
     vcf_reader = vcf.Reader(filename=snvs)
     snv_dtype = [('chrom','S50'),('pos',int),('gtype','S50'),('ref',float),('var',float)]
     snv_df = np.empty([0,5],dtype=snv_dtype)
@@ -347,7 +346,7 @@ def filter_germline(gml_file,sv_df,rlen,insert):
     print("Found %d SVs in the germline!" % len(germline))
     return sv_df.drop(germline,axis=0)
 
-def run(samples,svs,gml,cnvs,rlens,inserts,pis,ploidies,out,n_runs,num_iters,burn,thin,beta,neutral,snvs,snv_format,merge_clusts,use_map):
+def run(samples,svs,gml,cnvs,rlens,inserts,pis,ploidies,out,n_runs,num_iters,burn,thin,beta,neutral,snvs,snv_format,merge_clusts,use_map,cocluster):
 
     # pr = cProfile.Profile()
     # pr.enable()
@@ -375,8 +374,9 @@ def run(samples,svs,gml,cnvs,rlens,inserts,pis,ploidies,out,n_runs,num_iters,bur
                 snv_df = load_snvs_mutect(snvs,sample)
             elif snv_format == 'mutect_callstats':
                 snv_df = load_snvs_mutect_callstats(snvs)
-
-        filt_svs_file = '%s/%s_filtered_svs.tsv'%(out,sample)
+            n = snv_df['ref'].values
+            b = snv_df['var'].values
+    
         sv_df = load_svs(sv,rlen,insert)
 #        if os.path.isfile(filt_svs_file):
 #            print('Found filtered SVs file, running clustering on these variants')
@@ -412,8 +412,8 @@ def run(samples,svs,gml,cnvs,rlens,inserts,pis,ploidies,out,n_runs,num_iters,bur
         sv_df.index = range(len(sv_df))
         if len(snv_df)>0: snv_df.index = range(len(snv_df))
 
-        sv_df.index = range(len(sv_df))
         sv_df.to_csv('%s/%s_filtered_svs.tsv'%(out,sample),sep='\t',index=False,na_rep='')
+        snv_df.to_csv('%s/%s_filtered_snvs.tsv'%(out,sample),sep='\t',index=False,na_rep='')
         
         with open('%s/purity_ploidy.txt'%out,'w') as outf:
             outf.write("sample\tpurity\tploidy\n")
@@ -423,7 +423,7 @@ def run(samples,svs,gml,cnvs,rlens,inserts,pis,ploidies,out,n_runs,num_iters,bur
             print("Less than 5 post-filtered SVs. Clustering not recommended for this sample. Exiting.")
         else:
             print('Clustering with %d SVs and %d SNVs'%(len(sv_df),len(snv_df)))        
-            run_clus.infer_subclones(sample,sv_df,pi,rlen,insert,ploidy,out,n_runs,num_iters,burn,thin,beta,snv_df,merge_clusts,use_map)
+            run_clus.infer_subclones(sample,sv_df,pi,rlen,insert,ploidy,out,n_runs,num_iters,burn,thin,beta,snv_df,merge_clusts,use_map,cocluster)
 
         # pr.disable()
         # s = StringIO.StringIO()

SVClone/run_clus.py
@@ -59,7 +59,7 @@ def plot_cluster_hist(clusters,assignments,df,pl,pi,rlen,clus_out_dir,are_snvs=F
     hist_plot = '%s/merged_cluster_hist'%clus_out_dir
     plt.savefig(hist_plot)
 
-def plot_clusters(center_trace,clusters,assignments,df,pl,pi,rlen,clus_out_dir,are_snvs=False):
+def plot_clusters(center_trace,clusters,assignments,snv_df,sv_df,pl,pi,rlen,clus_out_dir):
     fig, axes = plt.subplots(2, 1, sharex=False, sharey=False, figsize=(12.5,8))
 
     RGB_tuples = gen_new_colours(len(clusters))
@@ -109,7 +109,7 @@ def mean_confidence_interval(phi_trace, alpha=0.1):
     h = se * sp.stats.t._ppf((1+(1-alpha))/2., n-1)
     return round(m,4), round(m-h,4), round(m+h,4)
 
-def merge_clusters(clus_out_dir,df,clus_info,clus_merged,clus_members,merged_ids,cparams,num_iters,burn,thin,are_snvs=False):
+def merge_clusters(clus_out_dir,snv_df,sv_df,clus_info,clus_merged,clus_members,merged_ids,cparams,num_iters,burn,thin):
     '''
     cparams = [pi,rlen,insert,ploidy]
     '''
@@ -205,11 +205,11 @@ def merge_results(clus_merged, merged_ids, df_probs, ccert):
     return df_probs_new,ccert_new
 
 
-def run_clust(clus_out_dir,df,pi,rlen,insert,ploidy,num_iters,burn,thin,beta,merge_clusts,use_map,are_snvs=False):
-    mcmc = cluster.cluster(df,pi,rlen,insert,ploidy,num_iters,burn,thin,beta,use_map,are_snvs)
-    npoints = len(df.spanning.values) if not are_snvs else len(df['var'].values)
-
+def post_process_clusters(mcmc,sv_df,snv_df,merge_clusts,clus_out_dir,sample,pi,rlen):
+    #npoints = len(df.spanning.values) if not are_snvs else len(df['var'].values)
     # assign points to highest probabiliy cluster
+    npoints = len(snv_df) + len(sv_df)
+
     z_trace = mcmc.trace('z')[:]
     clus_counts = [np.bincount(z_trace[:,i]) for i in range(npoints)]
     clus_max_prob = [index_max(c) for c in clus_counts]
@@ -237,75 +237,108 @@ def run_clust(clus_out_dir,df,pi,rlen,insert,ploidy,num_iters,burn,thin,beta,mer
     df_probs = pd.DataFrame(clus_counts,dtype=float)[clus_ids].fillna(0)
     df_probs = df_probs.apply(lambda x: x/sum(x),axis=1)
     df_probs.columns = col_names
-    df_pos = []
-    if are_snvs:
-        df_pos = ['chrom','pos']
-    else:
-        df_pos = ['bp1_chr','bp1_pos','bp2_chr','bp2_pos']
-    df_probs = df[df_pos].join(df_probs)
 
     # cluster certainty
     clus_max_df = pd.DataFrame(clus_max_prob,columns=['most_likely_assignment'])
     phi_cols = ["average_ccf","90_perc_CI_lo","90_perc_CI_hi"]
     phi_matrix = pd.DataFrame(phis[:],index=clus_ids,columns=phi_cols).loc[clus_max_prob]
     phi_matrix.index = range(len(phi_matrix))
-    ccert = df[df_pos].join(clus_max_df).join(phi_matrix)
+    ccert = clus_max_df.join(phi_matrix)
     clus_info.index = range(len(clus_info))
     print(clus_info)
 
+    if len(snv_df)>0 and len(sv_df)==0:
+        # snvs only trace output
+        clus_out_dir = '%s/snvs'%clus_out_dir        
+    trace_out = 'premerge' if merge_clusts else ''
+    trace_out = '%s/%s'%(clus_out_dir,trace_out)
+    write_output.dump_trace(clus_info,center_trace,trace_out+'phi_trace.txt')
+    write_output.dump_trace(clus_info,z_trace,trace_out+'z_trace.txt')
+    
+    if len(clus_info)>1 and merge_clusts: 
+        #TODO: fix merging
+        print('Merge clusters feature has been temporarily removed')
+
+    # merge clusters
+#    if len(clus_info)>1 and merge_clusts:        
+#        clus_merged = pd.DataFrame(columns=clus_info.columns,index=clus_info.index)
+#        clus_merged, clus_members, merged_ids  = merge_clusters(clus_out_dir,snv_df,sv_df,clus_info,clus_merged,\
+#                clus_members,[],[pi,rlen,insert,ploidy],num_iters,burn,thin)
+#        
+#        if len(clus_merged)!=len(clus_info):
+#            clus_info = clus_merged
+#            df_probs, ccert = merge_results(clus_merged, merged_ids, df_probs, ccert)
+#            if cmd.plot:
+#                plot_cluster_hist(clus_merged.clus_id.values,ccert.most_likely_assignment.values,df,ploidy,pi,rlen,clus_out_dir,are_snvs)
+    
+    snv_probs = pd.DataFrame()
+    snv_ccert = pd.DataFrame()
+    snv_members = np.empty(0)
+    if len(snv_df)>0:
+        snv_pos = ['chrom','pos']
+        snv_probs = df_probs.loc[:len(snv_df)-1]
+        snv_probs = snv_df[snv_pos].join(snv_probs)
+        
+        snv_ccert = ccert.loc[:len(snv_df)-1]
+        snv_ccert = snv_df[snv_pos].join(snv_ccert)
+
+        snv_max_probs = np.array(clus_max_prob)[:len(snv_df)]
+        snv_members = np.array([np.where(np.array(snv_max_probs)==i)[0] for i in clus_ids])
+
+    sv_probs = pd.DataFrame()
+    sv_ccert = pd.DataFrame()
+    sv_members = np.empty(0)
+    if len(sv_df)>0:
+        lb = len(snv_df) if len(snv_df)>0 else 0
+        
+        sv_pos = ['bp1_chr','bp1_pos','bp2_chr','bp2_pos']
+        sv_probs = df_probs.loc[lb:lb+len(sv_df)-1]
+        sv_probs.index = sv_df.index
+        sv_probs = sv_df[sv_pos].join(sv_probs)
+        
+        sv_ccert = ccert.loc[lb:lb+len(sv_df)-1]
+        sv_ccert.index = sv_df.index
+        sv_ccert = sv_df[sv_pos].join(sv_ccert)
+        
+        sv_max_probs = np.array(clus_max_prob)[:len(sv_df)]
+        sv_members = np.array([np.where(np.array(sv_max_probs)==i)[0] for i in clus_ids])
+    
+    if len(clus_info) < 1:
+        print("Warning! Could not converge on any major SV clusters. Skipping.\n")
+    else:
+        if len(snv_df)>0:
+            write_output.write_out_files_snv(snv_df,clus_info,snv_members,
+                    snv_probs,snv_ccert,clus_out_dir,sample,pi)
+        if len(sv_df)>0:
+            write_output.write_out_files(sv_df,clus_info,sv_members,
+                    sv_probs,sv_ccert,clus_out_dir,sample,pi,rlen)
+    
     # cluster plotting
     if cmd.plot:
         plot_clusters(center_trace,clus_idx,clus_max_prob,df,ploidy,pi,rlen,clus_out_dir,are_snvs)
     
-    trace_out = 'premerge'%clus_out_dir if merge_clusts else ''
-    write_output.dump_trace(clus_info,center_trace,clus_out_dir,trace_out+'phi_trace.txt',are_snvs)
-    write_output.dump_trace(clus_info,z_trace,clus_out_dir,trace_out+'z_trace.txt',are_snvs)
-    
-    # merge clusters
-    if len(clus_info)>1 and merge_clusts:        
-        clus_merged = pd.DataFrame(columns=clus_info.columns,index=clus_info.index)
-        clus_merged, clus_members, merged_ids  = merge_clusters(clus_out_dir,df,clus_info,clus_merged,\
-                clus_members,[],[pi,rlen,insert,ploidy],num_iters,burn,thin,are_snvs)
-        
-        if len(clus_merged)!=len(clus_info):
-            clus_info = clus_merged
-            df_probs, ccert = merge_results(clus_merged, merged_ids, df_probs, ccert)
-            if cmd.plot:
-                plot_cluster_hist(clus_merged.clus_id.values,ccert.most_likely_assignment.values,df,ploidy,pi,rlen,clus_out_dir,are_snvs)
-    
-    return clus_info,center_trace,z_trace,clus_members,df_probs,ccert
 
-def infer_subclones(sample,df,pi,rlen,insert,ploidy,out,n_runs,num_iters,burn,thin,beta,snv_df,merge_clusts,use_map):
+#return clus_info,center_trace,z_trace,clus_members,df_probs,ccert
+
+def infer_subclones(sample,sv_df,pi,rlen,insert,ploidy,out,n_runs,num_iters,burn,thin,beta,snv_df,merge_clusts,use_map,cocluster):
     clus_info,center_trace,ztrace,clus_members = None,None,None,None
     for i in range(n_runs):
         print("Cluster run: %d"%i)
 
         clus_out_dir = '%s/run%d'%(out,i)
-        
         if not os.path.exists(clus_out_dir):
             os.makedirs(clus_out_dir)    
-    
-        if len(snv_df)>0:
-            if not os.path.exists('%s/snvs'%clus_out_dir):
-                os.makedirs('%s/snvs'%clus_out_dir)
-            print("Clustering SNVs...")
-            clus_info,center_trace,z_trace,clus_members,df_probs,clus_cert = \
-                run_clust(clus_out_dir,snv_df,pi,rlen,insert,ploidy,num_iters,burn,thin,beta,merge_clusts,use_map,are_snvs=True)
-            write_output.write_out_files_snv(snv_df,clus_info,clus_members,df_probs,clus_cert,clus_out_dir,sample,pi)
-
-        print("Clustering SVs...")
-        clus_info,center_trace,z_trace,clus_members,df_probs,clus_cert = \
-                run_clust(clus_out_dir,df,pi,rlen,insert,ploidy,num_iters,burn,thin,beta,use_map,merge_clusts)
-         
-        sv_loss = 1-(sum(clus_info['size'])/float(len(df)))
-
-        if len(clus_info) < 1:
-            print("Warning! Could not converge on any major clusters. Skipping.\n")
-            continue
-            #if sv_loss > param.tolerate_svloss:
-            #    warn_str = "Warning! Lost %f of SVs.\n" % sv_loss
-            #if (clus_info.phi.values[0]*2.)>(1+param.subclone_diff):
-            #    warn_str = warn_str + "Warning! Largest VAF cluster exceeds 0.5.\n"
         
-        write_output.write_out_files(df,clus_info,clus_members,df_probs,clus_cert,clus_out_dir,sample,pi,rlen)
+        if cocluster:
+            mcmc = cluster.cluster(sv_df,snv_df,pi,rlen,insert,ploidy,num_iters,burn,thin,beta,use_map)
+            post_process_clusters(mcmc,sv_df,snv_df,merge_clusts,clus_out_dir,sample,pi,rlen)
+        else:            
+            if len(snv_df)>0:
+                if not os.path.exists('%s/snvs'%clus_out_dir):
+                    os.makedirs('%s/snvs'%clus_out_dir)
+                mcmc = cluster.cluster(pd.DataFrame(),snv_df,pi,rlen,insert,ploidy,num_iters,burn,thin,beta,use_map)
+                post_process_clusters(mcmc,pd.DataFrame(),snv_df,merge_clusts,clus_out_dir,sample,pi,rlen)
+            if len(sv_df)>0:
+                mcmc = cluster.cluster(sv_df,pd.DataFrame(),pi,rlen,insert,ploidy,num_iters,burn,thin,beta,use_map)
+                post_process_clusters(mcmc,sv_df,pd.DataFrame(),merge_clusts,clus_out_dir,sample,pi,rlen)
 

SVClone/write_output.py
@@ -7,10 +7,7 @@ from operator import methodcaller
 
 from . import cluster
 
-def dump_trace(clus_info,center_trace,clus_out_dir,trace_out,are_snvs):    
-    if are_snvs:
-        clus_out_dir = '%s/snvs'%clus_out_dir
-    outf = '%s/%s'%(clus_out_dir,trace_out)
+def dump_trace(clus_info,center_trace,outf):    
     traces = np.array([center_trace[:,cid] for cid in clus_info.clus_id.values])
     df_traces = pd.DataFrame(np.transpose(traces),columns=clus_info.clus_id)
     df_traces.to_csv(outf,sep='\t',index=False)
@@ -19,26 +16,42 @@ def write_out_files_snv(df,clus_info,clus_members,df_probs,clus_cert,clus_out_di
     clus_out_dir = '%s/snvs'%clus_out_dir
     if not os.path.exists(clus_out_dir):
         os.makedirs(clus_out_dir)
-    clus_info.to_csv('%s/clusters.txt'%(clus_out_dir),sep='\t',index=False)
+    
+    clus_info['phi'] = clus_info.phi.values*pi
+    clus_info = clus_info[['clus_id','size','phi']]
+    rename_cols =  {'clus_id': 'cluster', 'size': 'n_ssms', 'phi': 'proportion'}
+
+    clus_info = clus_info.rename(columns = rename_cols)    
+    clus_info.to_csv('%s/%s_subclonal_structure.txt'%(clus_out_dir,sample),sep='\t',index=False)
+
     with open('%s/number_of_clusters.txt'%clus_out_dir,'w') as outf:
         outf.write("sample\tclusters\n")
         outf.write('%s\t%d\n'%(sample,len(clus_info)))
     
-    cn_dtype =  [('chr1','S50'),
-                 ('pos1',int),
-                 ('total_copynumber1',int),
-                 ('no_chrs_bearing_mutation2',int)]
+    cn_dtype =  [('chr','S50'),
+                 ('pos',int),
+                 ('total_copynumber',int),
+                 ('no_chrs_bearing_mutation',int)]
 
-    mlcn_dtype =[('chrom','S50'),
+    mlcn_dtype =[('chr','S50'),
                  ('pos',int),
                  ('bb_CN','S50'),
                  ('most_likely_ref_copynumber',int),
                  ('most_likely_variant_copynumber',int),
                  ('prop_chrs_bearing_mutation',float)]
+    
+    mult_dtype =[('chr','S50'),
+                 ('pos',int),
+                 ('tumour_copynumber',int),
+                 ('multiplicity',int),
+                 ('tumour_copynumber_options','S50'),
+                 ('multiplicity_options','S50'),
+                 ('probabilities','S50')]
 
     cmem = np.hstack(clus_members)
     cn_vect = np.empty((0,len(cmem)),dtype=cn_dtype)
     mlcn_vect = np.empty((0,len(cmem)),dtype=mlcn_dtype)
+    mult_vect   = np.empty((0,len(cmem)),dtype=mult_dtype)
     #clus_snvs = df.loc[cmem].copy()
     df = df.fillna('')
     df = df[df.chrom.values!='']
@@ -68,13 +81,34 @@ def write_out_files_snv(df,clus_info,clus_members,df_probs,clus_cert,clus_out_di
         ml_new_row = np.array([(chrom,pos,snv['gtype'],ref_cn,sc_cn,freq)],dtype=mlcn_dtype)
         mlcn_vect = np.append(mlcn_vect,ml_new_row)
         
+        var_states = cn_states[idx]
+        tot_opts = ','.join(map(str,[int(x[1]) for x in var_states]))
+        var_opts = ','.join(map(str,[int(round(x[1]*x[2])) for x in var_states]))
+        probs = [(1/float(len(var_states)))]*len(var_states)
+        probs = ','.join(map(lambda x: str(round(x,4)),probs))
+
+        mult_new_row = np.array([(chrom,pos,sc_cn,int(round(freq*sc_cn)),tot_opts,var_opts,probs)],dtype=mult_dtype)
+        mult_vect = np.append(mult_vect,mult_new_row)
+        
+    clus_cert.average_ccf = clus_cert.average_ccf.values*pi
+    clus_cert['90_perc_CI_lo'] = clus_cert['90_perc_CI_lo'].values*pi
+    clus_cert['90_perc_CI_hi'] = clus_cert['90_perc_CI_hi'].values*pi
+
+    #rename cols
+    rename_cols =  {'bp1_chr': 'chr1', 'bp1_pos': 'pos1', 'bp2_chr': 'chr2', 
+                    'bp2_pos': 'pos2', 'average_ccf': 'average_proportion'}
+    clus_cert = clus_cert.rename(columns = rename_cols)
+    df_probs = df_probs.rename(columns = rename_cols)
+
     pd.DataFrame(mlcn_vect).to_csv('%s/%s_most_likely_copynumbers.txt'%(clus_out_dir,sample),sep='\t',index=False)
     pd.DataFrame(cn_vect).to_csv('%s/%s_copynumber.txt'%(clus_out_dir,sample),sep='\t',index=False)
     df_probs.to_csv('%s/%s_assignment_probability_table.txt'%(clus_out_dir,sample),sep='\t',index=False)
     clus_cert.to_csv('%s/%s_cluster_certainty.txt'%(clus_out_dir,sample),sep='\t',index=False)
+    pd.DataFrame(mult_vect).to_csv('%s/%s_multiplicity.txt'%(clus_out_dir,sample),sep='\t',index=False)
 
 def write_out_files(df,clus_info,clus_members,df_probs,clus_cert,clus_out_dir,sample,pi,rlen):
     
+    clus_info['phi'] = clus_info.phi.values*pi
     clus_info = clus_info[['clus_id','size','phi']]
     rename_cols =  {'clus_id': 'cluster', 'size': 'n_ssms', 'phi': 'proportion'}
 
@@ -125,8 +159,8 @@ def write_out_files(df,clus_info,clus_members,df_probs,clus_cert,clus_out_dir,sa
     clus_svs    = df.loc[cmem].copy()
     
     #sup,dep,cn_r,cn_v,mu_v,sides,av_cov = cluster.get_sv_vals(df,rlen)
-    sup,dep,combos,sides,Nvar = cluster.get_sv_vals(df,rlen)
-    cn_states = [cn[side] for cn,side in zip(combos,sides)]
+    sup,dep,cn_states,Nvar = cluster.get_sv_vals(df,rlen)
+    #TODO: should all be cellular prevalence - do this with SNVs also 
     phis = clus_cert.average_ccf.values
     cns,pvs = cluster.get_most_likely_cn_states(cn_states,sup,dep,phis,pi)
 
@@ -181,7 +215,7 @@ def write_out_files(df,clus_info,clus_members,df_probs,clus_cert,clus_out_dir,sa
     clus_cert['90_perc_CI_hi'] = clus_cert['90_perc_CI_hi'].values*pi
 
     #rename cols
-    rename_cols =  {'bp1_chr': 'chr1', 'bp1_pos': 'pos1', 'bp2_chr': 'chr2', 'bp2_pos': 'pos2'}
+    rename_cols =  {'bp1_chr': 'chr1', 'bp1_pos': 'pos1', 'bp2_chr': 'chr2', 'bp2_pos': 'pos2', 'average_ccf': 'average_proportion'}
     clus_cert = clus_cert.rename(columns = rename_cols)
     df_probs = df_probs.rename(columns = rename_cols)
 