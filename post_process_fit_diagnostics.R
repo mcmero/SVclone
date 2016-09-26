aa105758632e096ae65616e77c637c9c54782db3
Marek Cmero <cmerom@gmail.com>
2016-09-26 14:49:23 +1000

    Refactored post-processing script. Fixed CCF calculation.

--- post_process_fit_diagnostics.R
+++ post_process_fit_diagnostics.R
@@ -30,20 +30,232 @@ require(ggplot2)
 require(gridExtra)
 require(reshape)
 require(gtools)
+library(plyr)
+library(gtools)
 
-# Get number of runs
-runs <- c()
-for (dir in list.dirs()) {
-    cur_dir <- strsplit(dir,'/')[[1]]
-    if (length(cur_dir) <= 1)
-        next
-    cur_dir <- cur_dir[2]
-    if (substring(cur_dir,1,3) == 'run') {
-        runs <- c(runs,cur_dir)
+############################################################################################################################
+# Helper functions
+############################################################################################################################
+
+adjust_vafs <- function(dat, pur) {
+    mut_prop <- dat$prop_chrs_bearing_mutation
+    vaf <- dat$adjusted_vaf
+    frac <- dat$cn_frac
+
+    cn_v <- dat$most_likely_variant_copynumber * frac
+    cn_r <- dat$most_likely_ref_copynumber * (1 - frac)
+    cn_r <- sapply(cn_r,function(x){if(x==1){0}else{x}})
+
+    pn <- (1 - pur) * 2
+    pr <- pur * cn_r
+    pv <- pur * cn_v
+
+    norm_const <- pn + pr + pv
+    pv <- pv / norm_const
+
+    prob <- pv * mut_prop
+    adj_vaf <- (1 / prob) * vaf
+    adj_vaf[adj_vaf > 2] <- 2
+
+    return(adj_vaf)
+}
+
+get_frac <- function(x, snvs) {
+    variant_cn <- x['most_likely_variant_copynumber']
+    side <- x['preferred_side']
+    gtype <- x['gtype']
+    if (!snvs) {
+        if(side==0) {
+            gtype <- x['gtype1']
+        } else {
+            gtype <- x['gtype2']
+        }
+    }
+    sc <- strsplit(gtype, '\\|')[[1]]
+    sc <- strsplit(sc,',')
+    sc1 <- as.numeric(sc[[1]])
+    sc1 <- c(sum(sc1[1:2]),sc1[3])
+    if(length(sc) > 1) {
+        sc2 <- as.numeric(sc[[2]])
+        sc2 <- c(sum(sc2[1:2]),sc2[3])
+        if (sc1[1] == variant_cn) {
+            return(sc1[2])
+        } else {
+            return(sc2[2])
+        }
+    }
+    return(sc1[2])
+}
+
+get_runs <- function(wd) {
+    runs <- c()
+    for (dir in list.dirs(wd)) {
+        cur_dir <- strsplit(dir,'/')[[1]]
+        if (length(cur_dir) <= 1)
+            next
+        cur_dir <- cur_dir[length(cur_dir)]
+        if (substring(cur_dir,1,3) == 'run') {
+            runs <- c(runs,cur_dir)
+        }
+    }
+    return(unique(runs))
+}
+
+get_ic_table <- function(wd, base_name, runs, allowed_ics = c('BIC', 'AIC', 'AICc', 'svc_IC'), clus_penalty = 4, snvs = FALSE) {
+    snv_pref <- ''
+    samp_dir <- paste(wd, base_name, sep='/')
+
+    if (snvs) {snv_pref <- 'snvs/'}
+    ic_table <- NULL
+    for (run in runs) {
+        ic <- read.table(paste(samp_dir, '/', run, '/', snv_pref, base_name, '_fit.txt', sep=''), sep='\t', header=F, stringsAsFactors = F)
+        sc <- read.table(paste(samp_dir, '/', run, '/', base_name, '_subclonal_structure.txt', sep=''), sep='\t', header=T, stringsAsFactors = F)
+
+        accept_solution <- !(nrow(sc) == 1 & sc[1,'CCF'] < 0.9)
+        ic$V3 <- accept_solution
+
+        ic <- ic[ic$V1%in%allowed_ics,]
+        ic <- cbind(run=run, ic)
+        ic_table <- rbind(ic_table, ic)
+    }
+    ic_table$run <- factor(ic_table$run, levels=mixedsort(unique(as.character(ic_table$run))))
+    return(ic_table)
+}
+
+get_best_run <- function(wd, base_name, ic_metric, clus_penalty = 4) {
+    runs <- get_runs(wd)
+    ic <- get_ic_table(wd, base_name, runs, clus_penalty = clus_penalty)
+
+    min_ic <- min(ic[ic$V1==ic_metric,'V2'])
+    best_run <- as.character(ic[ic$V2==min_ic & ic$V1==ic_metric,'run'])[1]
+    return(best_run)
+}
+get_purity <- function(wd, base_name) {
+    pur <- read.table(paste(wd, base_name, '/purity_ploidy.txt', sep=''),
+                      header=T, sep='\t', stringsAsFactors=F)$purity
+    return(pur)
+}
+
+get_gtype <- function(x) {
+    if(as.numeric(x['frac1_A'])==1) {
+        return(paste(x['nMaj1_A'], x['nMin1_A'], x['frac1_A'],sep=','))
+    } else {
+        return(paste(paste(x['nMaj1_A'], x['nMin1_A'], x['frac1_A'],sep=','),
+                     paste(as.character(x['nMaj2_A']), as.character(x['nMin2_A']), as.character(x['frac2_A']),sep=','),sep='|'))
     }
 }
-runs <- unique(runs)
 
+############################################################################################################################
+# Plotting functions
+############################################################################################################################
+
+get_run_info <- function(wd, base_name, run, snvs = FALSE) {
+    snv_pref <- ''
+    if (snvs) {snv_pref <- 'snvs/'}
+    scs_file <-  paste(wd, base_name, '/', run, '/', snv_pref, base_name, '_subclonal_structure.txt', sep = '')
+    scs <- read.table(scs_file, sep = '\t', header = T)
+    #     scs <- scs[scs$n_ssms>3,]
+    scs <- scs[order(scs$CCF, decreasing=T), ]
+    scs$new_cluster <- 1:nrow(scs)
+
+    sv_df <- ''
+    if (snvs) {
+        sv_df <- read.table(paste(wd, base_name, '/', base_name, '_filtered_snvs.tsv', sep=''), header=T, sep='\t', stringsAsFactors=F)
+    } else {
+        sv_df <- read.table(paste(wd, base_name, '/', base_name, '_filtered_svs.tsv', sep=''), header=T, sep='\t', stringsAsFactors=F)
+    }
+    pur <- read.table(paste(wd, base_name, '/purity_ploidy.txt', sep=''),
+                      header=T, sep='\t', stringsAsFactors=F)$purity
+
+    cc_file <-  paste(wd, base_name, '/', run, '/', snv_pref, base_name, '_cluster_certainty.txt', sep = '')
+    cc <- read.table(cc_file, sep = '\t', stringsAsFactors = F, header = T)
+
+    mlcn_file <- paste(wd, base_name, '/', run, '/', snv_pref, base_name, '_most_likely_copynumbers.txt', sep='')
+    mlcn <- read.table(mlcn_file, header = T, sep = '\t', stringsAsFactors = F)
+
+    merge_cols <- c('chr1', 'pos1', 'dir1', 'chr2', 'pos2', 'dir2')
+    if (snvs) {
+        colnames(sv_df)[1] <- 'chr'
+        colnames(cc)[1] <- 'chr'
+        merge_cols <- c('chr', 'pos')
+    }
+    dat <- merge(sv_df, mlcn, by.x=merge_cols, by.y=merge_cols)
+    dat <- merge(dat, cc, by.x=merge_cols, by.y=merge_cols)
+    dat$cn_frac <- apply(dat, 1, function(x){get_frac(x, snvs)})
+    if(snvs) {dat$adjusted_vaf <- dat$var / (dat$ref + dat$var)}
+    dat <- cbind(dat, CCF=adjust_vafs(dat, pur))
+
+    dat$cluster <- NA
+    for (j in 1:nrow(scs)) {
+        dat[dat$most_likely_assignment==scs$cluster[j], 'cluster'] <- scs$new_cluster[j]
+    }
+    if (snvs) {
+        dat$sv <- paste(dat$chr, dat$pos, sep='_')
+    } else {
+        dat$sv <- paste(dat$chr1, dat$pos1, dat$dir1, dat$chr2, dat$pos2, dat$dir2, sep=':')
+    }
+    dat <- dat[!duplicated(dat),]
+    scs$cluster <- scs$new_cluster
+    scs <- scs[,1:4]
+    scs$variant_proportion <- scs$n_ssms/sum(scs$n_ssms)
+    return(list(dat,scs))
+}
+
+plot_ccf_hist <- function(wd, base_name, snvs, pick_run='best') {
+    if (pick_run=='best') {
+        pick_run <- get_best_run(wd, base_name, 'svc_IC')
+    }
+
+    x <- get_run_info(wd, base_name, pick_run, snvs)
+    dat <- x[[1]]
+    sc <- x[[2]]
+    sc$cluster <- sc$cluster-1
+    sc$clus_renum <- rank(sc$CCF)
+
+    dat$clus_renum <- NULL
+    for (i in 1:nrow(sc)) {
+        dat[dat$most_likely_assignment==sc$cluster[i], 'clus_renum'] <- sc[i,'clus_renum']
+    }
+
+    pp <- read.delim(paste(wd, base_name, '/purity_ploidy.txt', sep=''))
+    pur <- pp$purity
+
+    above_ssm_th <- sc$n_ssms / (sum(sc$n_ssms)) >= 0.04
+    below_ssm_th <- sc$n_ssms / (sum(sc$n_ssms)) < 0.04
+    clus_intercepts <- 1 / pur * as.numeric(sc$proportion[above_ssm_th & sc$n_ssms > 2])
+    clus_intercepts_minor <- 1 / pur * as.numeric(sc$proportion[below_ssm_th | sc$n_ssms<=2])
+
+    clus_freqs <- table(dat$most_likely_assignment)
+    minor_clusts <- names(clus_freqs[clus_freqs/nrow(dat) <= 0.04])
+
+    if (length(minor_clusts) != 0) {
+        dat <- dat[!dat$most_likely_assignment%in%minor_clusts,]
+    }
+
+    ccf_hist <- ggplot(dat, aes(x=as.numeric(dat$CCF),
+                                fill=factor(most_likely_assignment),color=factor(most_likely_assignment))) +
+        theme_minimal() +
+        xlim(0,2) + geom_histogram(alpha=0.3,position='identity',binwidth=0.05)+xlab('CCF') +
+        geom_vline(xintercept=clus_intercepts, colour='blue', size=1) + ylab('') + ylim(0,20) +
+        scale_fill_brewer(palette = 'Set1', name = "Cluster") +
+        scale_color_brewer(palette = 'Set1', name = "Cluster") +
+        theme(plot.title = element_text(size = 16, face = "bold"),
+              axis.title = element_text(size = 16),
+              axis.text.x = element_text(size = 14),
+              axis.text.y = element_text(size = 14)) +
+        ggtitle(pick_run)
+
+    if (length(clus_intercepts_minor) > 0) {
+        ccf_hist <- ccf_hist + geom_vline(xintercept=clus_intercepts_minor,colour='red',lty=2)
+    }
+    return(ccf_hist)
+}
+
+
+
+
+# Get number of runs
+runs <- get_runs('.')
 # cluster proportions plot
 clusts <- NULL
 for (run in runs) {
@@ -121,77 +333,44 @@ if (length(args)>2 & map) {
 # Plot histogram of clusters + QQ plots
 ############################################################################################
 
-get_adjust_factor <- function(dat, pur) {
-    cn_v <- dat$most_likely_variant_copynumber
-    cn_r <- dat$most_likely_ref_copynumber
-    mut_prop <- dat$prop_chrs_bearing_mutation
-    vaf <- dat$adjusted_vaf
-    frac <- dat$cn_frac
-    # prob <- (cn_v * mut_prop * pur) / (2 * (1 - pur) + cn_v * pur)
-    prob <- (cn_v * mut_prop * pur) / (2 * (1 - pur) + (cn_v * pur * frac) + (cn_r * pur * (1. - frac)))
-    return((1 / prob))
-}
+# get_adjust_factor <- function(dat, pur) {
+#     cn_v <- dat$most_likely_variant_copynumber
+#     cn_r <- dat$most_likely_ref_copynumber
+#     mut_prop <- dat$prop_chrs_bearing_mutation
+#     vaf <- dat$adjusted_vaf
+#     frac <- dat$cn_frac
+#     # prob <- (cn_v * mut_prop * pur) / (2 * (1 - pur) + cn_v * pur)
+#     prob <- (cn_v * mut_prop * pur) / (2 * (1 - pur) + (cn_v * pur * frac) + (cn_r * pur * (1. - frac)))
+#     return((1 / prob))
+# }
 
-var_file <- paste(id, '_filtered_svs.tsv', sep='')
-if (snvs) {
-    var_file <- paste(id, '_filtered_snvs.tsv', sep='')
-}
-svs <- read.table(var_file, header=T, sep='\t', stringsAsFactors=F)
-pur <- read.table('purity_ploidy.txt',header=T, sep='\t', stringsAsFactors=F)$purity
+# gg_color_hue <- function(n) {
+#     hues = seq(15, 375, length=n+1)
+#     hcl(h=hues, l=65, c=100)[1:n]
+# }
 
-gg_color_hue <- function(n) {
-    hues = seq(15, 375, length=n+1)
-    hcl(h=hues, l=65, c=100)[1:n]
-}
-
-ggQQ <- function(dat) {
-    p <- ggplot(dat) +
-        stat_qq(aes(sample=CCF, colour = factor(most_likely_assignment)), alpha = 0.5)
-
-    dat_tmp <- dat[!is.na(dat$CCF),]
-
-    clusts <- as.numeric(names(table(dat_tmp$most_likely_assignment)))
-    cols <- gg_color_hue(length(clusts))
-
-    for (i in 1:length(clusts)) {
-        clus <- clusts[i]
-        tmp <- dat_tmp[dat_tmp$most_likely_assignment == clus, 'CCF']
-        y <- quantile(tmp, c(0.25, 0.75))
-        x <- qnorm(c(0.25, 0.75))
-        slope <- diff(y)/diff(x)
-        intercept <- y[1L] - slope * x[1L]
-
-        p <- p + geom_abline(slope = slope, intercept = intercept, color=cols[i], alpha=0.5)
-    }
-
-    return(p)
-}
-
-get_frac <- function(x, snvs) {
-    variant_cn <- x['most_likely_variant_copynumber']
-    gtype <- x['gtype']
-    if (!snvs) {
-        side <- x['preferred_side']
-        gtype <- x['gtype1']
-        if(side==1) {
-            gtype <- x['gtype2']
-        }
-    }
-    sc <- strsplit(gtype, '\\|')[[1]]
-    sc <- strsplit(sc,',')
-    sc1 <- as.numeric(sc[[1]])
-    sc1 <- c(sum(sc1[1:2]),sc1[3])
-    if(length(sc) > 1) {
-        sc2 <- as.numeric(sc[[2]])
-        sc2 <- c(sum(sc2[1:2]),sc2[3])
-        if (sc1[1] == variant_cn) {
-            return(sc1[2])
-        } else {
-            return(sc2[2])
-        }
-    }
-    return(sc1[2])
-}
+# ggQQ <- function(dat) {
+#     p <- ggplot(dat) +
+#         stat_qq(aes(sample=CCF, colour = factor(most_likely_assignment)), alpha = 0.5)
+#
+#     dat_tmp <- dat[!is.na(dat$CCF),]
+#
+#     clusts <- as.numeric(names(table(dat_tmp$most_likely_assignment)))
+#     cols <- gg_color_hue(length(clusts))
+#
+#     for (i in 1:length(clusts)) {
+#         clus <- clusts[i]
+#         tmp <- dat_tmp[dat_tmp$most_likely_assignment == clus, 'CCF']
+#         y <- quantile(tmp, c(0.25, 0.75))
+#         x <- qnorm(c(0.25, 0.75))
+#         slope <- diff(y)/diff(x)
+#         intercept <- y[1L] - slope * x[1L]
+#
+#         p <- p + geom_abline(slope = slope, intercept = intercept, color=cols[i], alpha=0.5)
+#     }
+#
+#     return(p)
+# }
 
 if (map) {
     ic_table <- NULL
@@ -209,48 +388,24 @@ if (map) {
 }
 
 for (run in runs) {
-    mlcn <- read.table(paste(run, '/', snv_dir, id, '_most_likely_copynumbers.txt', sep=''), header=T, sep='\t', stringsAsFactors=F)
-    dat <- NULL
-    if (snvs) {
-        dat <- merge(svs, mlcn, by.x=c(1,2), by.y=c(1,2))
-        certain <- read.table(paste(run, '/', snv_dir, id, '_cluster_certainty.txt', sep=''), sep='\t', header=T)
-        dat <- merge(dat, certain,by.x=c(1,2), by.y=c(1,2))
-        dat$adjusted_vaf <- dat$var / (dat$ref + dat$var)
-    } else {
-        merge_cols <- c('chr1', 'pos1', 'dir1', 'chr2', 'pos2', 'dir2')
-        dat <- merge(svs, mlcn, by.x=merge_cols, by.y=merge_cols)
-        certain <- read.table(paste(run, '/', id, '_cluster_certainty.txt', sep=''), sep='\t', header=T)
-        dat <- merge(dat, certain,by.x=merge_cols, by.y=merge_cols)
-    }
+    plot1 <- plot_ccf_hist('../', id, snvs, run)
 
-    dat$cn_frac <- apply(dat, 1, function(x){get_frac(x, snvs)})
-    dat <- cbind(dat, CCF=get_adjust_factor(dat, pur) * dat$adjusted_vaf)
-    dat$CCF <- sapply(dat$CCF,function(x){min(2,x)})
-
-    sv_clust <- read.table(paste(run, '/', snv_dir, id, '_subclonal_structure.txt', sep=''), header=T, sep='\t', stringsAsFactors=F)
-#     sv_clust <- sv_clust[sv_clust$n_ssms>1, ]
-#     dat <- dat[dat$most_likely_assignment%in%sv_clust$cluster,]
-
-    above_ssm_th <- sv_clust$n_ssms / (sum(sv_clust$n_ssms)) >= 0.05
-    below_ssm_th <- sv_clust$n_ssms / (sum(sv_clust$n_ssms)) < 0.05
-    clus_intercepts <- 1 / pur * as.numeric(sv_clust$proportion[above_ssm_th & sv_clust$n_ssms > 2])
-    clus_intercepts_minor <- 1 / pur * as.numeric(sv_clust$proportion[below_ssm_th | sv_clust$n_ssms<=2])
-# 
-    plot1 <- ggplot(dat, aes(x=as.numeric(dat$CCF),
-                    fill=factor(most_likely_assignment), color=factor(most_likely_assignment))) +
-                    xlim(0,2) + geom_histogram(alpha=0.3,position='identity',binwidth=0.05)+xlab('CCF') +
-                    geom_vline(xintercept=clus_intercepts, colour='blue', size=1)
-
-    if (length(clus_intercepts_minor) > 0) {
-        plot1 <- plot1 + geom_vline(xintercept=clus_intercepts_minor,colour='red',lty=2)
-    }
-
+    x <- get_run_info('../', id, run, snvs = snvs)
+    dat <- x[[1]]
     plot2 <- ggplot(dat, aes(x=CCF, y=adjusted_vaf,
                              fill=factor(most_likely_assignment),
                              colour=factor(most_likely_assignment))) +
+                    scale_fill_brewer(palette = 'Set1', name = "Cluster") +
+                    scale_color_brewer(palette = 'Set1', name = "Cluster") +
+                    theme(plot.title = element_text(size = 16, face = "bold"),
+                          axis.title = element_text(size = 16),
+                          axis.text.x = element_text(size = 14),
+                          axis.text.y = element_text(size = 14)) +
                     geom_point(size=1) + xlim(0,2) + ylim(0,1) + ylab('VAF')
 
     # attach table for convenience, also add BIC/AIC
+    sv_clust <- read.table(paste(run, '/', snv_dir, id, '_subclonal_structure.txt', sep=''),
+                           header=T, sep='\t', stringsAsFactors=F)
     tabout <- sv_clust[order(as.numeric(sv_clust[,3]),decreasing=TRUE),]
     sc_tab <- tableGrob(tabout, rows=c())
     if (map) {
@@ -260,70 +415,16 @@ for (run in runs) {
         colnames(ic)[1] <- 'value'
         ic$value <- round(ic$value, 4)
         ic_tab <- tableGrob(ic)
-        height <- 7+round(nrow(tabout)*0.2)
-        pdf(paste(id, run, 'fit.pdf',sep='_'), height=height)
-        grid.arrange(arrangeGrob(sc_tab, ic_tab, nrow=1), plot1, plot2, ncol=1)
+        height <- 5+round(nrow(tabout)*0.2)
+        pdf(paste(id, run, 'fit.pdf',sep='_'), height=height, width=9)
+        grid.arrange(sc_tab, plot1, ic_tab, plot2, ncol=2)
         dev.off()
     } else {
         height <- 7+round(nrow(tabout)*0.2)
-        pdf(paste(id, run, 'fit.pdf',sep='_'), height=height)
+        pdf(paste(id, run, 'fit.pdf',sep='_'), height=height, width=5)
         grid.arrange(sc_tab, plot1, plot2, ncol=1)
         dev.off()
     }
-}
-
-get_run_info <- function(id, run) {
-    scs_file <- paste(run, '/', snv_dir, id, '_subclonal_structure.txt', sep='')
-    scs <- read.table(scs_file, sep = '\t', header = T)
-    # scs <- scs[scs$n_ssms>3,]
-    scs <- scs[order(scs$CCF, decreasing=T), ]
-    scs$new_cluster <- 1:nrow(scs)
-
-    sv_df <- NULL
-    if (snvs) {
-        sv_df <- read.table(paste(id, '_filtered_snvs.tsv', sep=''),
-                            header=T, sep='\t', stringsAsFactors=F)
-    } else {
-        sv_df <- read.table(paste(id, '_filtered_svs.tsv', sep=''),
-                            header=T, sep='\t', stringsAsFactors=F)
-    }
-    pur <- read.table(paste('purity_ploidy.txt', sep=''),
-                      header=T, sep='\t', stringsAsFactors=F)$purity
-
-    cc_file <-  paste(run, '/', snv_dir, id, '_cluster_certainty.txt', sep = '')
-    cc <- read.table(cc_file, sep = '\t', stringsAsFactors = F, header = T)
-
-    mlcn_file <- paste(run, '/', snv_dir, id, '_most_likely_copynumbers.txt', sep='')
-    mlcn <- read.table(mlcn_file, header = T, sep = '\t', stringsAsFactors = F)
-
-    merge_cols <- c('chr1', 'pos1', 'dir1', 'chr2', 'pos2', 'dir2')
-    if (snvs) {
-        colnames(sv_df)[1] <- 'chr'
-        colnames(cc)[1] <- 'chr'
-        merge_cols <- c('chr', 'pos')
-    }
-    dat <- merge(sv_df, mlcn, by.x=merge_cols, by.y=merge_cols)
-    dat <- merge(dat, cc, by.x=merge_cols, by.y=merge_cols)
-    if (snvs) {
-        dat$adjusted_vaf <- dat$var / (dat$ref + dat$var)
-    }
-    dat$cn_frac <- apply(dat, 1, function(x){get_frac(x, snvs)})
-    dat <- cbind(dat, CCF=get_adjust_factor(dat, pur) * dat$adjusted_vaf)
-
-    dat$cluster <- NA
-    for (j in 1:nrow(scs)) {
-        dat[dat$most_likely_assignment==scs$cluster[j], 'cluster'] <- scs$new_cluster[j]
-    }
-    if (snvs) {
-        dat$sv <- paste(dat$chr, dat$pos, sep='_')
-    } else {
-        dat$sv <- paste(dat$chr1, dat$pos1, dat$chr2, dat$pos2, sep=':')
-    }
-
-    scs$cluster <- scs$new_cluster
-    scs <- scs[,1:4]
-    scs$variant_proportion <- scs$n_ssms/sum(scs$n_ssms)
-    return(list(dat,scs))
 }
 
 var_id <- c('chr1','pos1','chr2','pos2')
@@ -332,7 +433,7 @@ if (snvs) {var_id <- c('chr', 'pos')}
 all_runs_ccfs <- NULL
 all_runs_scs <- NULL
 for(run in runs) {
-    runinf <- get_run_info(id, run)
+    runinf <- get_run_info('../', id, run, snvs)
     dat <- runinf[[1]]
     scs <- runinf[[2]]
 
