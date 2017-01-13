76c2e4b5cd8b6591fb91be6756314fe86862afc6
Marek Cmero <cmerom@gmail.com>
2017-01-13 14:53:25 +1100

    Added new config file for running example; svclone_config now contains recommended settings

--- run_example.sh
+++ run_example.sh
@@ -4,13 +4,13 @@ input=example_data/tumour_p80_DEL_svs_simple.txt
 bam=example_data/tumour_p80_DEL_sv_extract_sorted.bam
 sample=tumour_p80_DEL
 
-./SVclone.py annotate -i $input -b $bam -s $sample --sv_format simple
+./SVclone.py annotate -i $input -b $bam -s $sample --sv_format simple -cfg svclone_test.ini
 
-./SVclone.py count -i ${sample}/${sample}_svin.txt -b $bam -s $sample
+./SVclone.py count -i ${sample}/${sample}_svin.txt -b $bam -s $sample -cfg svclone_test.ini
 
-./SVclone.py filter -s $sample -i ${sample}/${sample}_svinfo.txt -p example_data/purity_ploidy.txt
+./SVclone.py filter -s $sample -i ${sample}/${sample}_svinfo.txt -p example_data/purity_ploidy.txt -cfg svclone_test.ini
 
-./SVclone.py cluster -s $sample
+./SVclone.py cluster -s $sample -cfg svclone_test.ini
 
 Rscript post_process_fit_diagnostics.R $sample $sample
 
