31b02a7a7960b41dccbed196d67a05ca3a9d1a73
Marek Cmero <mcmero@student.unimelb.edu.au>
2017-06-01 10:55:37 +1000

    Minor improvements for running example data

--- run_example.sh
+++ run_example.sh
@@ -12,12 +12,16 @@ sample=tumour_p80_DEL
 
 ./SVclone.py cluster -s $sample -cfg svclone_test.ini
 
-Rscript post_process_fit_diagnostics.R $sample $sample
+./SVclone.py post_assign -s $sample --svs ${sample}/${sample}_svinfo.txt -cfg svclone_test.ini
+
+Rscript post_process_fit_diagnostics.R $sample $sample --map
 
 # uncomment below to test run with SNVs:
 
 #./SVclone.py filter -s $sample -i ${sample}/${sample}_svinfo.txt -p example_data/purity_ploidy.txt --snvs example_data/tumour_p80_DEL_snvs.vcf --snv_format mutect
 
-#./SVclone.py cluster -s $sample
+#./SVclone.py cluster -s $sample -cfg svclone_test.ini
 
-#Rscript post_process_fit_diagnostics.R $sample $sample --snvs
+#./SVclone.py post_assign -s $sample --svs ${sample}/${sample}_svinfo.txt -cfg svclone_test.ini --snvs example_data/tumour_p80_DEL_    snvs.vcf --snv_format mutect
+
+#Rscript post_process_fit_diagnostics.R $sample $sample --colcus --map
